68677501fc1514bd162a91a7ad1b47632885ec8b
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-12 16:03:31 +0700

    deklarasi prosedur tambahAnak() dan cetakAnak()

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -13,17 +13,38 @@ class ibu
 public:
     //deklarasi member variabel pada class ibu
     string nama; 
+    vector<anak*> daftar_anak;
     
     //membuat constructor dan destructor pada class ibu
     ibu(string pnama) : nama(pnama) 
     {
         cout << "Ibu \"" << nama << "\" ada \n";
-    }
+    };
     
     ~ibu() 
     {
         cout << "Ibu \"" << nama << "\" tidak ada \n";
-    }
+    };
+
+    // deklarasi prosedur tambahAnak() dan cetakAnak()
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
 };
+
+// pendefinisian prosedur - prosedur pada class ibu diluar class
+void ibu::tambahAnak(anak* pAnak)
+{
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak()
+{
+    cout << "Daftar anak dari Ibu \"" << this->nama << "\" : \n";
+    for (auto& a : daftar_anak)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 
 #endif 
